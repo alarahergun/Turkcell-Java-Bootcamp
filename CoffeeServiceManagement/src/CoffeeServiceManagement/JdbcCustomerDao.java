0c4fc65a04c3fd2e45924662c10a3cb692e7a896
alarahergun <57047231+alarahergun@users.noreply.github.com>
2021-01-26 14:28:09 +0300

    Update JdbcCustomerDao.java

--- CoffeeServiceManagement/src/CoffeeServiceManagement/JdbcCustomerDao.java
+++ CoffeeServiceManagement/src/CoffeeServiceManagement/JdbcCustomerDao.java
@@ -4,25 +4,25 @@ public class JdbcCustomerDao implements ICustomerDao{
 
 	@Override
 	public void add() {
-		System.out.println("Müþteri JDBC ile eklendi.");
+		System.out.println("MÃ¼steri JDBC ile eklendi.");
 		System.out.println();
 	}
 
 	@Override
 	public void update() {
-		System.out.println("Müþteri JDBC ile güncellendi.");
+		System.out.println("MÃ¼steri JDBC ile gÃ¼ncellendi.");
 		System.out.println();
 	}
 
 	@Override
 	public void delete() {
-		System.out.println("Müþteri JDBC ile silindi.");
+		System.out.println("MÃ¼steri JDBC ile silindi.");
 		System.out.println();
 	}
 
 	@Override
 	public void verify() {
-		System.out.println("Müþteri JDBC ile doðrulandý.");
+		System.out.println("MÃ¼steri JDBC ile dogrulandi.");
 		System.out.println();
 	}
 
