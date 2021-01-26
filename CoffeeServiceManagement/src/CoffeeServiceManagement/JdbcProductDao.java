a2c5d1eb5fb9490974df8fe8e68365db9d6d9adf
alarahergun <57047231+alarahergun@users.noreply.github.com>
2021-01-26 14:28:40 +0300

    Update JdbcProductDao.java

--- CoffeeServiceManagement/src/CoffeeServiceManagement/JdbcProductDao.java
+++ CoffeeServiceManagement/src/CoffeeServiceManagement/JdbcProductDao.java
@@ -4,19 +4,19 @@ public class JdbcProductDao implements IProductDao{
 
 	@Override
 	public void add() {
-		System.out.println("Kampanya JDBC ile güncellendi.");
+		System.out.println("Kampanya JDBC ile eklendi.");
 		System.out.println();
 	}
 
 	@Override
 	public void delete() {
-		System.out.println("Kampanya JDBC ile güncellendi.");
+		System.out.println("Kampanya JDBC ile silindi.");
 		System.out.println();
 	}
 
 	@Override
 	public void update() {
-		System.out.println("Kampanya JDBC ile güncellendi.");
+		System.out.println("Kampanya JDBC ile gÃ¼ncellendi.");
 		System.out.println();
 	}
 
